38b9728a95b008287c780dd8de050f696d13b9dc
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-28 14:54:48 +0530

    Use-case1, Test-case1

--- Program.cs
+++ Program.cs
@@ -6,26 +6,32 @@ namespace GenericProblems
     {
         public static void Main(string[] args)
         {
-            int[] intArray = { 1, 2, 3, 4 };
-            //double[] doubleArray = { 1.01, 2.02, 3.03, 4.04 };
-            //char[] charArray = { 'H', 'A', 'P', 'P', 'Y'};
-            //Program.toPrint<int>(intArray);
-            //Program.toPrint<double>(doubleArray);
-            //Program.toPrint<char>(charArray);
+            //int[] intArray = { 1, 2, 3, 4 };
+            ////double[] doubleArray = { 1.01, 2.02, 3.03, 4.04 };
+            ////char[] charArray = { 'H', 'A', 'P', 'P', 'Y'};
+            ////Program.toPrint<int>(intArray);
+            ////Program.toPrint<double>(doubleArray);
+            ////Program.toPrint<char>(charArray);
 
-            Console.WriteLine("Please enter the First number");
+            //Console.WriteLine("Please enter the First number");
 
-            int num1 = Convert.ToInt32(Console.ReadLine());
-
-
-            Console.WriteLine("Please enter the Second number");
-
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num1 = Convert.ToInt32(Console.ReadLine());
 
 
-            Console.WriteLine("Please enter the Third number");
+            //Console.WriteLine("Please enter the Second number");
 
-            int num3 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine("Please enter the Third number");
+
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+            //**************************************************************************
+            //Usecase 1.1
+            int num1 = 54; 
+            int num2 = 53; 
+            int num3 = 23;
+
             FindMaximum obj = new FindMaximum();
             int value = FindMaximum.FindingMaximum(num1, num2, num3);
             Console.WriteLine("{0} is the Greater Value", value);
